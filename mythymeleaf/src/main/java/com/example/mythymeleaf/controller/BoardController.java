ba23b27c614bfef00648f313836836d972092e9d
hyun6ik <fkanwm@naver.com>
2021-01-27 18:05:03 +0900

    thymeleaf에서 form 전송하기 (Validate 추가)

--- mythymeleaf/src/main/java/com/example/mythymeleaf/controller/BoardController.java
+++ mythymeleaf/src/main/java/com/example/mythymeleaf/controller/BoardController.java
@@ -2,12 +2,15 @@ package com.example.mythymeleaf.controller;
 
 import com.example.mythymeleaf.domain.Board;
 import com.example.mythymeleaf.repository.BoardRepository;
+import com.example.mythymeleaf.validator.BoardValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,10 +20,36 @@ public class BoardController {
 
     private final BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model){
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards",boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String from(Model model, @RequestParam(required = false) Long id){
+
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String Submit(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
+
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- mythymeleaf/src/main/java/com/example/mythymeleaf/domain/Board.java
+++ mythymeleaf/src/main/java/com/example/mythymeleaf/domain/Board.java
@@ -1,24 +1,27 @@
 package com.example.mythymeleaf.domain;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Board {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다")
     private String title;
+
     private String content;
 
 
-    public Board(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
+
 }
