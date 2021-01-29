18b0c64c412ee0cc27373fcce2f059f98f68b015
hyun6ik <fkanwm@naver.com>
2021-01-29 11:07:16 +0900

    게시판 검색 처리

--- mythymeleaf/src/main/java/com/example/mythymeleaf/repository/BoardRepository.java
+++ mythymeleaf/src/main/java/com/example/mythymeleaf/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.example.mythymeleaf.repository;
 
 import com.example.mythymeleaf.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +13,6 @@ public interface BoardRepository extends JpaRepository<Board,Long> {
 
     List<Board> findByTitleOrContent(String title, String content);
 
-
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 
 }
