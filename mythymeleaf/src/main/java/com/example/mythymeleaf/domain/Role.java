921d84fea8c53799be135479f8e39f0753225ed9
hyun6ik <fkanwm@naver.com>
2021-02-08 23:23:20 +0900

    도메인 모델 생성

---
+++ mythymeleaf/src/main/java/com/example/mythymeleaf/domain/Role.java
@@ -0,0 +1,24 @@
+package com.example.mythymeleaf.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+}
---
+++ mythymeleaf/src/main/java/com/example/mythymeleaf/domain/User.java
@@ -0,0 +1,28 @@
+package com.example.mythymeleaf.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password;
+    private Boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles = new ArrayList<>();
+}
