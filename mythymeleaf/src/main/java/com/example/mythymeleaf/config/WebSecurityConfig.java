99d9c383f098bcc52637ba28d0794bfdd15247da
hyun6ik <fkanwm@naver.com>
2021-02-08 22:59:49 +0900

    로그인 페이지 작성

--- mythymeleaf/src/main/java/com/example/mythymeleaf/config/WebSecurityConfig.java
+++ mythymeleaf/src/main/java/com/example/mythymeleaf/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/login")
+                    .loginPage("/account/login")
                     .permitAll()
                     .and()
                 .logout()
